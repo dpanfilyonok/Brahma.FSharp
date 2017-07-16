89a990fd783cd2eaa6ccb368eb33c314378f9ffc
gsv <rsdpisuy@gmail.com>
2017-07-16 15:58:51 +0300

    Bump version to 1.1.3

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,3 +1,6 @@
+### 1.1.3
+* Fix OpenCL compiller options.
+
 ### 1.1.2
 * More diagnostic information in kernel compiler
 
--- src/Brahman.Substrings/AssemblyInfo.fs
+++ src/Brahman.Substrings/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Brahman.Substrings")>]
 [<assembly: AssemblyProductAttribute("Brahma.FSharp")>]
 [<assembly: AssemblyDescriptionAttribute("F# quotation to OpenCL translator.")>]
-[<assembly: AssemblyVersionAttribute("1.1.2")>]
-[<assembly: AssemblyFileVersionAttribute("1.1.2")>]
+[<assembly: AssemblyVersionAttribute("1.1.3")>]
+[<assembly: AssemblyFileVersionAttribute("1.1.3")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Brahman.Substrings"
     let [<Literal>] AssemblyProduct = "Brahma.FSharp"
     let [<Literal>] AssemblyDescription = "F# quotation to OpenCL translator."
-    let [<Literal>] AssemblyVersion = "1.1.2"
-    let [<Literal>] AssemblyFileVersion = "1.1.2"
+    let [<Literal>] AssemblyVersion = "1.1.3"
+    let [<Literal>] AssemblyFileVersion = "1.1.3"
