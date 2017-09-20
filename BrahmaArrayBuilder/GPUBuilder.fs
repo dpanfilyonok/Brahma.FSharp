15b2bb8a6990819fb60a1e8f405ec63589e7db1b
VaseninaAnna <saibrog@yandex.ru>
2017-09-20 09:56:26 +0300

    Created a file for the builder implementation

---
+++ BrahmaArrayBuilder/GPUBuilder.fs
@@ -0,0 +1,29 @@
+﻿open ArrayGPU
+open Brahma.Helpers
+open OpenCL.Net
+open Brahma.OpenCL
+open Brahma.FSharp.OpenCL.Core
+open Microsoft.FSharp.Quotations
+open Brahma.FSharp.OpenCL.Extensions 
+
+type BrahmaBuilder () =
+  let platformName = "NVIDIA*"
+  let deviceType = DeviceType.Default        
+    
+  let provider =
+        try  ComputeProvider.Create(platformName, deviceType)
+        with 
+        | ex -> failwith ex.Message
+  let commandQueue = new CommandQueue(provider, provider.Devices |> Seq.head) 
+  
+  member this.Zero() =
+        printfn "Zero"
+        None    
+   
+  member this.Return (x) =
+    commandQueue.Dispose()
+    provider.Dispose()
+    provider.CloseAllBuffers()
+    x
+
+let Indus = new BrahmaBuilder()
