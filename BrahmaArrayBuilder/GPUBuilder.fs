f44d9b2cd4b2aae2f1016b3a6ce5a7f4f0d8c4ab
VaseninaAnna <saibrog@yandex.ru>
2017-10-11 11:39:54 +0300

    Builder implementation

--- BrahmaArrayBuilder/GPUBuilder.fs
+++ BrahmaArrayBuilder/GPUBuilder.fs
@@ -5,71 +5,18 @@ open Microsoft.FSharp.Quotations
 open Brahma.FSharp.OpenCL.Extensions 
 open ArrayGPU
 
-
 type Reader<'environment,'a> = Reader of ('environment -> 'a)
-
-
-
 type ReaderM<'d,'out> = 
     'd -> 'out
 
 module Reader =
-
-    // basic operations
 
     let run dep (rm : ReaderM<_,_>) =
         rm dep
 
     let constant (c : 'c) : ReaderM<_,'c> =
         fun _ -> c
-
-    // lifting of functions and state
-
-    let lift1 (f : 'd -> 'a -> 'out)
-              : 'a -> ReaderM<'d, 'out> =
-        fun a dep -> f dep a
-
-    let lift2 (f : 'd -> 'a -> 'b -> 'out)
-              : 'a -> 'b -> ReaderM<'d, 'out> =
-        fun a b dep -> f dep a b
-
-    let lift3 (f : 'd -> 'a -> 'b -> 'c -> 'out)
-              : 'a -> 'b -> 'c -> ReaderM<'d, 'out> =
-        fun a b c dep -> f dep a b c
-
-    let liftDep (proj : 'd2 -> 'd1) 
-                (rm : ReaderM<'d1, 'output>) 
-                : ReaderM<'d2, 'output> =
-        proj >> rm
-            
-    // functor
-
-    let fmap (f : 'a -> 'b) 
-             (g : 'c -> 'a) 
-             : ('c -> 'b) =
-        g >> f
-
-    let map (f : 'a -> 'b) 
-            (rm : ReaderM<'d, 'a>) 
-            : ReaderM<'d,'b> =
-        rm >> f
-
-    let (<?>) = map
-
-    // applicative-functor
-        
-    let apply (f : ReaderM<'d, 'a->'b>)
-              (rm : ReaderM<'d, 'a>)
-              : ReaderM<'d, 'b> =
-        fun dep ->
-            let f' = run dep f
-            let a  = run dep rm
-            f' a
-
-    let (<*>) = apply
-
-    // monad
-
+    
     let bind (rm : ReaderM<'d, 'a>) 
              (f : 'a -> ReaderM<'d,'b>) 
              : ReaderM<'d, 'b> =
@@ -81,32 +28,30 @@ module Reader =
 
 module BrahmaBuilder =
     open Reader
-
+           
     type context = (ComputeProvider * CommandQueue * int * int)
-    type Context<'a> = Reader<context, 'a>
+    
+    //We grant named access to the elements of the context tuple
+    let prov(x:context) =
+        match x with
+            |(a,b,c,d) -> a
+    let CQ (x:context) =
+        match x with
+            |(a,b,c,d) -> b
+    let len(x:context) =
+        match x with
+            |(a,b,c,d) -> c
+    let WS(x:context) =
+        match x with
+            |(a,b,c,d) -> d
 
-    and BrahmaBuilder (length: int) =
-   
-        //defining context
-        let platformName = "NVIDIA*"
-        let deviceType = DeviceType.Default        
-        let provider =
-                try  ComputeProvider.Create(platformName, deviceType)
-                with 
-                | ex -> failwith ex.Message
-        let mutable commandQueue = new CommandQueue(provider, provider.Devices |> Seq.head) 
-        let localWorkSize = 
-            let lws, ex = OpenCL.Net.Cl.GetDeviceInfo(provider.Devices |> Seq.head, OpenCL.Net.DeviceInfo.MaxWorkGroupSize)
-            let maxWorkSize = int <| lws.CastTo<uint64>()
-            if length <= maxWorkSize then length
-            else 
-                let mutable l = maxWorkSize
-                while (length % l <> 0) do 
-                    l <- l - 1
-                l
-        let (actcontext: context) = (provider, commandQueue, length, localWorkSize)
+    //Implementing a builder, using the methods from Reader Monad
+    type BrahmaBuilder (actcontext: context) =     
+        let mutable provider = prov actcontext
+        let mutable commandQueue = CQ actcontext
+        let mutable length = len actcontext 
+        let mutable localWorkSize = WS actcontext
         
-
         member __.Bind(m, f)    = m >>= f
         member __.Return (outArr: array<_>) =
             let _ = commandQueue.Add(outArr.ToHost provider).Finish()
@@ -117,24 +62,89 @@ module BrahmaBuilder =
         member __.Delay(f)      = f ()
     
 
+    
+module tests =
+    open BrahmaBuilder
+    //defining context
+    let platformName = "NVIDIA*"
+    let deviceType = DeviceType.Default        
+    let provider1 =
+                try  ComputeProvider.Create(platformName, deviceType)
+                with 
+                | ex -> failwith ex.Message
+    let mutable commandQueue1 = new CommandQueue(provider1, provider1.Devices |> Seq.head) 
+    let length1 = 5
+    
+    let getLocalWorkSize1 (length) = 
+            let lws, ex = OpenCL.Net.Cl.GetDeviceInfo(provider1.Devices |> Seq.head, OpenCL.Net.DeviceInfo.MaxWorkGroupSize)
+            let maxWorkSize = int <| lws.CastTo<uint64>()
+            if length <= maxWorkSize then length
+            else 
+                let mutable l = maxWorkSize
+                while (length % l <> 0) do 
+                    l <- l - 1
+                l
+    
+    let localworksize1 = getLocalWorkSize1 length1
+    
 
+    let actcontext = provider1, commandQueue1, length1, localworksize1
+       
+    let a = [|5; 7; 8; 22; 16|] 
+        
+    let Indus = new BrahmaBuilder(actcontext)
+    let computation1 =
+             Indus 
+                 { 
+                                 
+                     let! c = ArrayGPU.Reverse a
+                     let! d = ArrayGPU.Map <@ fun a -> a + 1 @> c
+                     return d
+                   } 
+        
+    //Now we have to unwrap the value which we get from the computation
+    //To do that we use the run function from the Reader module with the same context that we use in the computation
+    let test1 = Reader.run actcontext computation1
+    let printresult result = printfn "result=%A" result
+    printresult test1
+    //We can see, that it works just as expected
+    //We get the result1 equil to [|17; 23; 9; 8; 6|]
 
-    let Indus = new BrahmaBuilder(5)
+    let y = 1
+    
+    let provider2 =
+                try  ComputeProvider.Create(platformName, deviceType)
+                with 
+                | ex -> failwith ex.Message
+    let mutable commandQueue2 = new CommandQueue(provider1, provider1.Devices |> Seq.head) 
+    let length2 = 6
+    
+    let getLocalWorkSize2 (length) = 
+            let lws, ex = OpenCL.Net.Cl.GetDeviceInfo(provider2.Devices |> Seq.head, OpenCL.Net.DeviceInfo.MaxWorkGroupSize)
+            let maxWorkSize = int <| lws.CastTo<uint64>()
+            if length <= maxWorkSize then length
+            else 
+                let mutable l = maxWorkSize
+                while (length % l <> 0) do 
+                    l <- l - 1
+                l
+    let localworksize2 = getLocalWorkSize2 length2
+    
 
-    let test1 =
-      Indus 
-         { 
-            let a = [|5, 7, 8, 22, 16|]
-            let! b = ArrayGPU.Reverse a 
-//            let! c = ArrayGPU.Map <@
-//                                     fun (rng:_1D) (a:array<_>) ->
-//                                     let i = rng.GlobalID0
-//                                     if (i >= 0) && (i <= 3)
-//                                     then a.[i] <- 0 @> b
-            return b
-         } 
+    let actcontext2 = provider2, commandQueue2, length2, localworksize2
+    let Indus2=BrahmaBuilder(actcontext2)
 
-
+    let computation2 =
+             Indus2 
+                 { 
+                     //let a = [|5; 7; 8; 22; 16|]             
+                     let! c = ArrayGPU.Reverse a
+                     //let! d = c1 c 
+                     return c
+                   } 
+    let test2 = Reader.run actcontext computation2
+      //If we try to compose it with the same computation we get an error about the wrong worksize
+    
 
 
  
