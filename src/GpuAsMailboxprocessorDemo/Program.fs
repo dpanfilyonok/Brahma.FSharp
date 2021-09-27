08e1bb29c6dd67705c1211f890234bef8c34a21e
gsv <rsdpisuy@gmail.com>
2021-09-27 11:11:53 +0300

    Demo project removed.

--- src/GpuAsMailboxprocessorDemo/Program.fs
+++
@@ -1,7 +0,0 @@
-﻿[<EntryPoint>]
-let main argv =
-    printfn "Started!"
-    let host = new Brahma.FSharp.OpenCL.Host()
-    let res = host.Do(argv)
-    printfn "res = %A" res
-    0 // return an integer exit code
