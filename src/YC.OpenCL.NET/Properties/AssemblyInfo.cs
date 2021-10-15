a57364dbf3f0d0bf91a5f3710389d50bf81da18a
gsv <rsdpisuy@gmail.com>
2021-10-15 10:23:31 +0300

    Version up.

--- src/YC.OpenCL.NET/Properties/AssemblyInfo.cs
+++ src/YC.OpenCL.NET/Properties/AssemblyInfo.cs
@@ -5,20 +5,20 @@ using System.Reflection;
 [assembly: AssemblyTitle("YC.OpenCL.NET")]
 [assembly: AssemblyProduct("Brahma.FSharp")]
 [assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyMetadata("ReleaseDate","2021-09-27T00:00:00.0000000+03:00")]
+[assembly: AssemblyMetadata("ReleaseDate","2021-10-15T00:00:00.0000000+03:00")]
 [assembly: AssemblyFileVersion("2.0.0")]
 [assembly: AssemblyInformationalVersion("2.0.0")]
 [assembly: AssemblyMetadata("ReleaseChannel","alpha")]
-[assembly: AssemblyMetadata("GitHash","d3459373555b2712152de070508329991fb830ef")]
+[assembly: AssemblyMetadata("GitHash","d6f607b9f46e076ba8a13bac5387e06d379669f8")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyTitle = "YC.OpenCL.NET";
         internal const System.String AssemblyProduct = "Brahma.FSharp";
         internal const System.String AssemblyVersion = "2.0.0";
-        internal const System.String AssemblyMetadata_ReleaseDate = "2021-09-27T00:00:00.0000000+03:00";
+        internal const System.String AssemblyMetadata_ReleaseDate = "2021-10-15T00:00:00.0000000+03:00";
         internal const System.String AssemblyFileVersion = "2.0.0";
         internal const System.String AssemblyInformationalVersion = "2.0.0";
         internal const System.String AssemblyMetadata_ReleaseChannel = "alpha";
-        internal const System.String AssemblyMetadata_GitHash = "d3459373555b2712152de070508329991fb830ef";
+        internal const System.String AssemblyMetadata_GitHash = "d6f607b9f46e076ba8a13bac5387e06d379669f8";
     }
 }
