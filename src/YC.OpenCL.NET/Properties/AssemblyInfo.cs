8dab10f8007a260da88dc0ba1ef67ba64f337ec7
gsv <rsdpisuy@gmail.com>
2022-07-05 12:19:22 +0300

    Bump version to 2.0.0

--- src/YC.OpenCL.NET/Properties/AssemblyInfo.cs
+++ src/YC.OpenCL.NET/Properties/AssemblyInfo.cs
@@ -5,20 +5,20 @@ using System.Reflection;
 [assembly: AssemblyTitle("YC.OpenCL.NET")]
 [assembly: AssemblyProduct("Brahma.FSharp")]
 [assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyMetadata("ReleaseDate","2021-11-26T00:00:00.0000000+03:00")]
+[assembly: AssemblyMetadata("ReleaseDate","2022-07-05T00:00:00.0000000+03:00")]
 [assembly: AssemblyFileVersion("2.0.0")]
 [assembly: AssemblyInformationalVersion("2.0.0")]
-[assembly: AssemblyMetadata("ReleaseChannel","alpha9")]
-[assembly: AssemblyMetadata("GitHash","17f05e40eac8cda8b7bec37f4db25f14189e2600")]
+[assembly: AssemblyMetadata("ReleaseChannel","release")]
+[assembly: AssemblyMetadata("GitHash","1ce449225b33424a6bf15719a96e26f9bb8268bb")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyTitle = "YC.OpenCL.NET";
         internal const System.String AssemblyProduct = "Brahma.FSharp";
         internal const System.String AssemblyVersion = "2.0.0";
-        internal const System.String AssemblyMetadata_ReleaseDate = "2021-11-26T00:00:00.0000000+03:00";
+        internal const System.String AssemblyMetadata_ReleaseDate = "2022-07-05T00:00:00.0000000+03:00";
         internal const System.String AssemblyFileVersion = "2.0.0";
         internal const System.String AssemblyInformationalVersion = "2.0.0";
-        internal const System.String AssemblyMetadata_ReleaseChannel = "alpha9";
-        internal const System.String AssemblyMetadata_GitHash = "17f05e40eac8cda8b7bec37f4db25f14189e2600";
+        internal const System.String AssemblyMetadata_ReleaseChannel = "release";
+        internal const System.String AssemblyMetadata_GitHash = "1ce449225b33424a6bf15719a96e26f9bb8268bb";
     }
 }
--- tests/Brahma.FSharp.Tests/AssemblyInfo.fs
+++ tests/Brahma.FSharp.Tests/AssemblyInfo.fs
@@ -5,19 +5,19 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Brahma.FSharp.Tests")>]
 [<assembly: AssemblyProductAttribute("Brahma.FSharp")>]
 [<assembly: AssemblyVersionAttribute("2.0.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","2021-11-26T00:00:00.0000000+03:00")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","2022-07-05T00:00:00.0000000+03:00")>]
 [<assembly: AssemblyFileVersionAttribute("2.0.0")>]
 [<assembly: AssemblyInformationalVersionAttribute("2.0.0")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseChannel","alpha9")>]
-[<assembly: AssemblyMetadataAttribute("GitHash","17f05e40eac8cda8b7bec37f4db25f14189e2600")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseChannel","release")>]
+[<assembly: AssemblyMetadataAttribute("GitHash","1ce449225b33424a6bf15719a96e26f9bb8268bb")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Brahma.FSharp.Tests"
     let [<Literal>] AssemblyProduct = "Brahma.FSharp"
     let [<Literal>] AssemblyVersion = "2.0.0"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "2021-11-26T00:00:00.0000000+03:00"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "2022-07-05T00:00:00.0000000+03:00"
     let [<Literal>] AssemblyFileVersion = "2.0.0"
     let [<Literal>] AssemblyInformationalVersion = "2.0.0"
-    let [<Literal>] AssemblyMetadata_ReleaseChannel = "alpha9"
-    let [<Literal>] AssemblyMetadata_GitHash = "17f05e40eac8cda8b7bec37f4db25f14189e2600"
+    let [<Literal>] AssemblyMetadata_ReleaseChannel = "release"
+    let [<Literal>] AssemblyMetadata_GitHash = "1ce449225b33424a6bf15719a96e26f9bb8268bb"
